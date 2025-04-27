dd3cb81152bd7a288ca5e74747d7dda194009012
jordankamto <jordankamto32@gmail.com>
2025-04-27 11:31:05 +0100

    udpated getTodos controller to get all todos

--- dist/controllers/todo.js
+++ dist/controllers/todo.js
@@ -1,23 +1,55 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.deleteTodo = exports.updateTodo = exports.addTodo = exports.getTodo = exports.getTodos = void 0;
-const getTodos = (req, res, next) => {
-    res.status(200).json({ message: "Get all todos" });
-};
+const todo_1 = __importDefault(require("../models/todo"));
+const getTodos = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const todos = yield todo_1.default.find();
+        if (todos.length === 0) {
+            const error = {
+                message: "No Todos found",
+                status: 404,
+                data: "",
+            };
+            throw error;
+        }
+        res
+            .status(200)
+            .json({ message: "All Todos were fetched successfully", data: todos });
+    }
+    catch (error) {
+        if (!error.status) {
+            error.status = 500;
+        }
+        next(error);
+    }
+});
 exports.getTodos = getTodos;
-const getTodo = (req, res, next) => {
+const getTodo = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     res.status(200).json({ message: "Get a todo by id" });
-};
+});
 exports.getTodo = getTodo;
-const addTodo = (req, res, next) => {
+const addTodo = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     res.status(200).json({ message: "Add a todo" });
-};
+});
 exports.addTodo = addTodo;
-const updateTodo = (req, res, next) => {
+const updateTodo = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     res.status(200).json({ message: "Update a todo by id" });
-};
+});
 exports.updateTodo = updateTodo;
-const deleteTodo = (req, res, next) => {
+const deleteTodo = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     res.status(200).json({ message: "Delete a todo by id" });
-};
+});
 exports.deleteTodo = deleteTodo;
--- src/controllers/todo.ts
+++ src/controllers/todo.ts
@@ -1,19 +1,86 @@
 import { Request, Response, NextFunction } from "express";
 import Todo from "../models/todo";
 import { validationResult } from "express-validator";
+import { CustomError } from "../types/types";
 
-export const getTodos = (req: Request, res: Response, next: NextFunction) => {
-  res.status(200).json({ message: "Get all todos" });
+export const getTodos = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const todos = await Todo.find();
+    if (todos.length === 0) {
+      const error: CustomError = {
+        message: "No Todos found",
+        status: 404,
+        data: "",
+      };
+      throw error;
+    }
+    res
+      .status(200)
+      .json({ message: "All Todos were fetched successfully", data: todos });
+  } catch (error) {
+    if (!(error as CustomError).status) {
+      (error as CustomError).status = 500;
+    }
+    next(error);
+  }
 };
-export const getTodo = (req: Request, res: Response, next: NextFunction) => {
-  res.status(200).json({ message: "Get a todo by id" });
+
+export const getTodo = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+  } catch (error) {
+    if (!(error as CustomError).status) {
+      (error as CustomError).status = 500;
+    }
+    next(error);
+  }
 };
-export const addTodo = (req: Request, res: Response, next: NextFunction) => {
-  res.status(200).json({ message: "Add a todo" });
+
+export const addTodo = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+  } catch (error) {
+    if (!(error as CustomError).status) {
+      (error as CustomError).status = 500;
+    }
+    next(error);
+  }
 };
-export const updateTodo = (req: Request, res: Response, next: NextFunction) => {
-  res.status(200).json({ message: "Update a todo by id" });
+
+export const updateTodo = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+  } catch (error) {
+    if (!(error as CustomError).status) {
+      (error as CustomError).status = 500;
+    }
+    next(error);
+  }
 };
-export const deleteTodo = (req: Request, res: Response, next: NextFunction) => {
-  res.status(200).json({ message: "Delete a todo by id" });
+
+export const deleteTodo = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+  } catch (error) {
+    if (!(error as CustomError).status) {
+      (error as CustomError).status = 500;
+    }
+    next(error);
+  }
 };
